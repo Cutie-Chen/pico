9e1b6f0a44912451fbb48c88a719cba3d735cffa
panpan1229 <yfyang1132@163.com>
2024-11-25 15:51:50 +0800

    coinScene done

---
+++ Assets/Scripts/CoinManager.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class CoinManager : MonoBehaviour
+{
+    public GameObject[] cubes;    // 所有金币
+    public GameObject[] numbers;  // 对应的数字
+    private int currentIndex = 0; // 当前显示的金币索引
+
+    void Start()
+    {
+        // 初始化时，所有金币和数字都隐藏
+        foreach (GameObject cube in cubes)
+        {
+            cube.SetActive(false);
+        }
+        foreach (GameObject number in numbers)
+        {
+            number.SetActive(false);
+        }
+        cubes[currentIndex].SetActive(true);
+    }
+
+    // 控制显示下一个金币
+    public void ShowNextCoin(int coinIndex)
+    {
+        if (coinIndex == currentIndex)
+        {
+            // 显示当前金币并隐藏数字
+            cubes[coinIndex].SetActive(true);
+            numbers[coinIndex].SetActive(false);
+
+            // 更新索引，准备显示下一个金币
+            currentIndex++;
+
+            // 如果还有更多的金币，显示下一个金币
+            if (currentIndex < cubes.Length)
+            {
+                cubes[currentIndex].SetActive(true);
+            }
+        }
+    }
+}
--- Assets/Scripts/trigger_detect.cs
+++ Assets/Scripts/trigger_detect.cs
@@ -4,33 +4,52 @@ using UnityEngine;
 
 public class trigger_detect : MonoBehaviour
 {
-    bool shouldRotate = false;
+    bool shouldRotate = true;
     public GameObject cube;
+    public GameObject number;
     public Vector3 rotationSpeed = new Vector3(0, 100, 0);
-    void Update()
+    public int coinIndex;   //当前金币的索引
+    private CoinManager coinManager;
+
+    private void Start()
     {
-        // 如果触发器内停留，旋转物体
+        coinManager = FindObjectOfType<CoinManager>();
+    }
+
+    private void Update()
+    {
+        // 如果需要旋转，才进行旋转
         if (shouldRotate)
         {
             cube.transform.Rotate(rotationSpeed * Time.deltaTime);
         }
     }
+
     private void OnTriggerEnter(Collider collision)
     {
         Debug.Log("进入了碰撞");
-        cube.SetActive(true);
+        //cube.SetActive(true);
     }
 
     private void OnTriggerStay(Collider collision)
     {
         Debug.Log("碰撞停留");
-        shouldRotate = true;
+        //用户按键后，金币变成数字，目前仅写了键盘事件
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            cube.SetActive(false);
+            number.SetActive(true);
+            shouldRotate=false;
+            
+            coinManager.ShowNextCoin(coinIndex);    //显示下一个金币
+        }
     }
 
-    private void OnTriggerExit(Collider collision)
+    /*private void OnTriggerExit(Collider collision)
     {
         Debug.Log("碰撞离开");
         cube.SetActive(false);
         shouldRotate = false;
     }
+    */
 }
